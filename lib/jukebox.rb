a2dd45d80211733443b9530e90af671545975cd2
Austen Bright <pureaustenbright@gmail.com>
2020-04-19 23:50:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -40,8 +40,8 @@ end
 
 def run(songs)
   puts "Please enter a command:"
-  input = gets.strip
   until input == "exit"
+    input = gets.strip
     if input == "exit"
       exit_jukebox
     elsif input == "list"
